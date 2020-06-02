849c4df53a04d08d00d732c387a7bbf2e3e0931f
NayRSilva <nayarasilva@cjr.org.br>
2020-06-02 15:48:23 -0300

    creating BarChart for States

--- src/views/Home/components/Estados.vue
+++ src/views/Home/components/Estados.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="Brasil">
-        teste
+    <div>
+        {{ dates }}
+        <region></region>
     </div>
 </template>
 
 <script>
+import Dados from "../../../example/index.js";
+import Region from "./charts/Region.vue"
+
 export default {
-    
+    components: { Region },
+    data(){
+        return {
+            data: Dados.DF,
+            dates: null
+        }
+    },
+    methods:{
+        filter_dates(){
+            let data = null
+            let retorno = []
+            for(let i = 0; i < this.data.length; i++){
+                data = this.data[i].dataExtracao
+                retorno.indexOf(data) === -1 ? retorno.push(data) : console.log("This item already exists");
+            }
+            this.dates = retorno
+        }
+    }
 }
 </script>
 
 <style scoped>
-.Brasil{
-    background-color: blue;
-}
 </style>
---
+++ src/views/Home/components/charts/BarChart.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="app">
+        <vue-apex-charts 
+            width="100%"
+            type="bar" 
+            height="800vh"
+            :options="options" 
+            :series="series"
+            :key="numId"
+        ></vue-apex-charts>
+    </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+    props: [ 
+        'infected', 'time', 'death', 'region'
+    ],
+    components: {
+        VueApexCharts
+    },
+    data(){
+        return {
+            selected: ['Infectados', 'Obitos'],
+            nulo: [0],
+            filtrado_inf: {
+                data: [],
+                time: [],
+                name: 'chart'
+            },
+            filtrado_obt: {
+                data: [],
+                time: [],
+                name: 'chart'
+            },
+            options: {
+                plotOptions: {
+                    bar: {
+                        horizontal: true,
+                        barHeight: '100%',
+                    }
+                },
+                colors: [ '#2E93fA', '#c21f1f' ],
+                chart: {
+                    id: 'vuechart-example'
+                },
+                xaxis: {
+                    categories: this.region
+                }
+            },
+            series: [
+                {
+                    name: 'Infectados',
+                    data: this.infected
+                },
+                {
+                    name: 'Óbitos',
+                    data: this.death
+                }
+            ],
+            numId: 0
+        }   
+    }
+}
+</script>
+
+<style scoped>
+#RA_name{
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 25px;
+}
+
+.selector{
+    align-content: center;
+    text-align: center;
+}
+</style>
